1c8caec1b20e046f448c94824e6457f7a493893b
Ivan Zavadsky <ivazin3@gmail.com>
2019-09-13 17:40:54 +0300

    Updated create_user_table migration

--- basic/migrations/m190913_093518_create_user_table.php
+++ basic/migrations/m190913_093518_create_user_table.php
@@ -13,11 +13,32 @@ class m190913_093518_create_user_table extends Migration
     public function safeUp()
     {
         $this->createTable('{{%user}}', [
-            'id'       => $this->primaryKey(),
-            'login'    => $this->string(),
-            'password' => $this->string(),
-            'role'     => $this->string(),
+            'id'          => $this->primaryKey(),
+            'username'    => $this->string(256),
+            'password'    => $this->string(256),
+            'auth_key'     => $this->string(256),
+            'access_token' => $this->string(256),
+            'role'        => $this->string(10),
         ]);
+
+        $this->insert('{{%user}}', [
+            'id'          => '100',
+            'username'    => 'admin',
+            'password'    => 'admin',
+            'auth_key'     => 'test100key',
+            'access_token' => '100-token',
+            'role'        => 'admin',
+        ]);
+
+        $this->insert('{{%user}}', [
+            'id'          => '101',
+            'username'    => 'demo',
+            'password'    => 'demo',
+            'auth_key'     => 'test101key',
+            'access_token' => '101-token',
+            'role'        => 'editor'
+        ]);
+
     }
 
     /**
